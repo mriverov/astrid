3be6ac3ea889f37ee8c0fd654b82337b061ef1e3
marinarv <marina.rivero@mulesoft.com>
2018-03-13 16:42:36 -0300

    add new examples with for and while

---
+++ examples/src/main/java/ExampleForWhile.java
@@ -0,0 +1,69 @@
+import java.util.List;
+
+/**
+ * Created by marina.rivero on 3/13/18.
+ */
+public class ExampleForWhile {
+
+    /**
+     * devuelve true si la variable toBeFound esta en la lista elements
+     * @param elements
+     * @param toBeFound
+     */
+    public void found(List<String> elements, String toBeFound){
+        boolean found = false;
+
+        for(String currentElement: elements){
+
+        }
+
+    }
+
+    /**
+     * devuelve true si la variable toBeFound esta en la lista elements
+     * @param elements
+     * @param toBeFound
+     */
+    public void found2(List<String> elements, String toBeFound){
+        boolean found = false;
+        boolean condition = false;
+
+        while(condition){
+
+        }
+
+    }
+
+    /**
+     * Devuelve dos listas, una con la primera mitad de elements y otra con al segunda.
+     * Si elements es [1,2,3,4,5,6]
+     * la funcion tiene que devolver [1,2,3] y [4,5,6]
+     * Si elements es [1,2,3,4,5]
+     * la funcion tiene que devolver [1,2,3] y [4,5]
+     * @param elements
+     */
+    public void split(List<String> elements){
+
+        for(String currentElement: elements){
+
+        }
+
+    }
+
+    /**
+     * Devuelve dos listas, una con la primera mitad de elements y otra con al segunda.
+     * Si elements es [1,2,3,4,5,6]
+     * la funcion tiene que devolver [1,2,3] y [4,5,6]
+     * Si elements es [1,2,3,4,5]
+     * la funcion tiene que devolver [1,2,3] y [4,5]
+     * @param elements
+     */
+    public void split2(List<String> elements){
+        boolean condition = false;
+
+        while(condition){
+
+        }
+
+    }
+}
